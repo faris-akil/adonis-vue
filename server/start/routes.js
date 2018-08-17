5e48aa3cd57fee1016a94844c37cd3f8c53ef8f1
Akil <akil8989@gmail.com.my>
2018-08-17 16:09:36 +0800

    register/login route

--- server/start/routes.js
+++ server/start/routes.js
@@ -15,6 +15,7 @@
 
 const Route = use('Route')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
+Route.group(() => {
+  Route.post("/auth/register", "UserController.register");
+  Route.post("/auth/login", "UserController.login");
+});
