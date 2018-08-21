d52811f0a8357678f34b2770cd6a431bbfea86f9
Akil <akil8989@gmail.com.my>
2018-08-21 10:47:06 +0800

    task index + creat

--- server/start/routes.js
+++ server/start/routes.js
@@ -23,4 +23,6 @@ Route.group(() => {
   Route.post("/projects", "ProjectController.create").middleware("auth");
   Route.delete("/projects/:id", "ProjectController.destroy").middleware("auth");
   Route.patch("/projects/:id", "ProjectController.update").middleware("auth");
+  Route.post("projects/:id/tasks", "TaskController.create").middleware("auth");
+  Route.get("projects/:id/tasks", "TaskController.index").middleware("auth");
 });
