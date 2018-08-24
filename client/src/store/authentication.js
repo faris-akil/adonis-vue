8036b23befe68f12e7c701d7cc26cf6fc6c1428a
Akil <akil8989@gmail.com.my>
2018-08-24 10:45:07 +0800

    register

---
+++ client/src/store/authentication.js
@@ -0,0 +1,49 @@
+import router from '../router';
+import HTTP from '../http';
+
+export default {
+  namespaced: true,
+  state: {
+    registerEmail: 'hello YOOOO',
+    registerPassword: 'world',
+    token: null,
+    registerError: null,
+  },
+  actions: {
+    logout({ commit }){
+      commit('setToken', null);
+      router.push('/login');
+    },
+    register({ commit, state }){
+      commit('setRegisterError', null);
+      return HTTP().post('auth/register', {
+        email: state.registerEmail,
+        password: state.registerPassword,
+      }).then(({ data }) => {
+        commit('setToken', data.token);
+        router.push('/');
+      }).catch(() => {
+        commit('setRegisterError', 'Invalid Registration Information');
+      });
+    },
+  },
+  getters: {
+    isLoggedIn(state){
+      return !!state.token;
+    }
+  },
+  mutations: {
+    setToken(state, token){
+      state.token = token;
+    },
+    setRegisterError(state, error){
+      state.registerError = error;
+    },
+    setRegisterEmail(state, email){
+      state.registerEmail = email;
+    },
+    setRegisterPassword(state, password){
+      state.registerPassword = password;
+    },
+  },
+};
