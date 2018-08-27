7b51b96f81552c5cc90165ad4271c11a0cb6e115
Akil <akil8989@gmail.com.my>
2018-08-27 17:48:33 +0800

    editable record and create record components

---
+++ client/src/components/CreateRecord.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <v-layout row wrap class="mt-4">
+    <v-flex xs8>
+      <v-text-field :placeholder="placeholder" @input="$emit('onInput', $event)" :value="value" @keyup.enter="$emit('create')">
+
+      </v-text-field>
+    </v-flex>
+    <v-flex xs4 class="text-xs-right">
+      <v-btn dark color="blue" class="mt-2" @click="$emit('create')">
+        <v-icon class="mr-2">add_circle</v-icon>
+        Create
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: [
+      'placeholder',
+      'value',
+  ],
+};
+</script>
+
+<style lang="css">
+</style>
---
+++ client/src/components/EditableRecord.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <v-layout row wrap>
+    <v-flex xs9 class="text-xs-left">
+      <span v-if="!isEditMode">
+        {{title}}
+      </span>
+      <v-text-field
+        v-if="isEditMode" :value="title" @keyup.enter="$emit('onSave')" @input="$emit('onInput', $event)">
+
+      </v-text-field>
+    </v-flex>
+    <v-flex xs3 class="text-xs-right">
+      <v-icon @click="$emit('onEdit')" v-if="!isEditMode">edit</v-icon>
+      <v-icon @click="$emit('onSave')" v-if="isEditMode">check</v-icon>
+      <v-icon @click="$emit('onDelete')" >delete</v-icon>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: [
+    'isEditMode',
+    'title',
+    'record',
+  ]
+};
+</script>
+
+<style lang="css">
+</style>
